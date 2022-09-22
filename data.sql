b663c2d627f53a8b66b63de85c90f83159c3f38f
Tuyishimire Eric <tuyishimireericc@gmail.com>
2022-09-22 19:22:09 +0200

    Added species_id and owners-id info

--- data.sql
+++ data.sql
@@ -16,3 +16,30 @@ values
 (9, 'Boarmon', '2005-06-07', 7, true, 20.4), 
 (10, 'Blossom', '1998-10-13', 3, true, 17), 
 (11, 'Ditto', '2022-05-14', 4, true, 22);
+
+
+/* query multiple tables */
+INSERT INTO owners (full_name, age)
+VALUES
+('Sam Smith', 34), 
+('Jennifer Orwell', 19),
+('Bob', 45), 
+('Melody Pond', 77), 
+('Dean Winchester', 14), 
+('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES ('pokemon'), ('digimon');
+
+UPDATE animals SET species_id = 2 WHERE name like '%mon';
+
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+
+UPDATE animals SET owners_id = 1 WHERE name = 'Agumon';
+
+UPDATE animals SET owners_id = 2 WHERE name = 'Gabumon' OR name = 'Pikachu';
+
+UPDATE animals SET owners_id = 3 WHERE name = 'Devimon' OR name = 'Plantmon';
+
+UPDATE animals SET owners_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals SET owners_id = 5 WHERE name IN ('Angemon', 'Boarmon');
